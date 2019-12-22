1d80fc0f06897164450a9f6c0b4242400958b378
livepo <notify@livepo.info>
2019-12-22 21:10:33 +0800

    valid parentheses

--- valid-parentheses.go
+++ valid-parentheses.go
@@ -1,26 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"container/list"
+)
 
 
-type Stack struct {
-	list []int
-}
-
-
-func NewStack() * Stack() {
-	list := make([]int, 0)
-	return $Stack{list: list}
-}
-
-
-
-func (s *Stack) Top() int {
-	length = len(s.list)
-	return s.list[length-1]
+func match(a, b rune) bool {
+	switch(b) {
+	case '}':
+		return a == '{'
+	case ')':
+		return a == '('
+	case ']':
+		return a == '['
+	}
+	return false
 }
 
 
 func isValid(s string) bool {
-	stack := 
+	stack := list.New()
+	for _, c := range s {
+		last := stack.Back()
+		if last == nil || !match(last.Value.(rune), c) {
+			stack.PushBack(c)
+		} else {
+			stack.Remove(stack.Back())
+		}
+	}
+	if stack.Len() != 0 {
+		return false
+	} else {
+		return true
+	}
+}
+
+
+func main() {
+	ret := isValid("({)}()")
+	fmt.Println(ret)
 }
